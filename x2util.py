b8941f41248e080e50254850133996d497a459b3
Jake Houser <jake@x2engine.com>
2016-05-20 11:17:33 -0700

    Remove dry-run flag from rsync

--- x2util.py
+++ x2util.py
@@ -251,7 +251,7 @@ def rsync_live_to_gitdir(options):
 	else:
 		file_path = options.directory+'/X2CRM/x2engine/'
 
-	cmd = ['rsync', '-avczOn','--delete', '--exclude-from', '.rsync_exclude', file_path, options.gitdir+'/X2CRM/x2engine/']
+	cmd = ['rsync', '-avczO','--delete', '--exclude-from', '.rsync_exclude', file_path, options.gitdir+'/X2CRM/x2engine/']
 	subprocess.check_call(cmd)
 
 	chset(options, {'u': old_u_val, 'D': old_D_val})
